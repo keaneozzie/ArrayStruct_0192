1933f0d5a0965dd596acf1b4106ed7c202f738b2
keaneozzie <ozziedharma897@gmail.com>
2026-04-06 16:30:08 +0700

    menampilkan  dengan menggunakan pengulangan for

--- array.cpp
+++ array.cpp
@@ -54,3 +54,10 @@ cin >> kamu[3];
     cout << endl;
     // Menampilkan dengan menggunakan perulangan for
 
+    //menampilakan dengan menggunakan pengulangan for
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke-" << i+1 << " = " << kamu[i] << endl;
+    }
+}
+
