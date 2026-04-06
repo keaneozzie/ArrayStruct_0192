83e09a951ea811087c6a69af603c2dbcfdf82a64
keaneozzie <ozziedharma897@gmail.com>
2026-04-06 16:33:54 +0700

    menampilkan int main

--- structNew2.cpp
+++ structNew2.cpp
@@ -15,3 +15,21 @@ struct Orang
     int umur;
 };
 
+int main() {
+    Orang mhs;
+    cout << "Nama : " ;
+    getline(cin, mhs.nama);
+    cout << "Alamat Desa: " ;
+    getline(cin, mhs.alamat.desa);
+    cout << "Alamat Kota: " ;
+    getline(cin, mhs.alamat.kota);
+    cout << "Umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    // tampilkan
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Alamat : " << mhs.alamat.desa << endl;
+    cout << " Alamat : " << mhs.alamat.kota << endl;
+    cout << " Umur : " << mhs.umur << endl;
+}
