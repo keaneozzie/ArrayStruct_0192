7eee72c90a5129df2a9b4c6147be1dafd7cdf75a
keaneozzie <ozziedharma897@gmail.com>
2026-04-06 15:29:24 +0700

    menambahkan int main

--- array.cpp
+++ array.cpp
@@ -1,3 +1,8 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+    // deklarasi array dengan inisialisasi 
+    int aku[5] = {10,20,30,40,50};
+    // deklarasi array tanpa inisialisasi
+    int kamu[5]
