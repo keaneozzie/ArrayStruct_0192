605553080b88f781955b062d7a90d42c876f2d51
keaneozzie <ozziedharma897@gmail.com>
2026-04-06 16:31:52 +0700

    menampilkan int main

--- structNew.cpp
+++ structNew.cpp
@@ -8,3 +8,18 @@ struct orang
     int umur;
 };
 
+int main(){
+    orang mhs;
+    cout << "nama : ";
+    cin >> mhs.nama;
+    cout << "alamat : ";
+    cin >> mhs.alamat;
+    cout << "umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    // tampilkan 
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "alamat : " << mhs.alamat << endl;
+    cout << "umur : " << mhs.umur << endl;
+}
